46d529b5e83cd3ce02815ca39037f4f35b002964
Manish <manishsharma3825@gmail.com>
2024-08-13 15:48:38 +0530

    updated for before submission

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
-import { bgColors } from "./styles/Theme";
-import { Box, styled, Grid } from "@mui/material";
+import { Box } from "@mui/material";
 import Header from "./component/Header";
-import Email from "./screens/email/Email";
 import PublicRoutes from "./routes/public route/PublicRoutes";
 
 function App() {
   return (
-    <Box className="h-screen">
+    <Box className="h-screen"  >
       <Header />
       <PublicRoutes />
     </Box>
